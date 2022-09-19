83176846f566792d179ccdca77d2cac771f8763c
RochaDanielPrado <112509637+RochaDanielPrado@users.noreply.github.com>
2022-09-19 12:00:45 -0300

    arquivo connect.py

--- connect.py
+++ connect.py
@@ -1,13 +1,15 @@
 import mysql.connector
 
-con = mysql.connector.connect(host='', database='', user='', password='')
+con = mysql.connector.connect(host='162.241.61.180',
+                              database='dexoveco_wp070',
+                              user='dexoveco_daniel', password='@Rocce1102')
 
 if con.is_connected():
     db_info = con.get_server_info()
     print('Conectado ao servidor MySQL versão , db_info')
     cursor = con.cursor()
     cursor.execute('Select database();')
-    linha = cursor.fechone()
+    linha = cursor.fetchone()
     print('Conectado ao banco de dados ', linha)
 
 if con.is_connected:
